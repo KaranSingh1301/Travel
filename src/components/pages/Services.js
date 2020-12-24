524643857547862e6d2af1538c126f236a125066
Karan singh <kssinghkaran13@gmail.com>
2020-12-24 22:04:38 +0530

    first commit

---
+++ src/components/pages/Services.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../../App.css'
+
+function Services() {
+const services={
+    backgroundImage:" url('https://scx2.b-cdn.net/gfx/news/hires/2018/2-ocean.jpg')",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+ 
+    color: "#fff",
+    fontSize: "100px",
+    height: "700px"
+}
+    return (
+        
+            
+  <h1 style={services}>SERVICES</h1>
+        
+     
+    )
+}
+
+export default Services
