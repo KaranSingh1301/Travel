ff1daf27179c4828f4f5f62989fff7c4725ee22f
gifty <jasmeetsingh200801@gmail.com>
2021-02-28 13:56:41 +0530

    did signUp validation

--- src/components/pages/SignUp/SignUp.js
+++ src/components/pages/SignUp/SignUp.js
@@ -2,44 +2,48 @@ import React from "react";
 import "./SignUp.css";
 import { useFormik } from "formik";
 
-const validate = (values) => {
-  const errors = {};
-
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address";
-  }
-
-  if (!values.username) {
-    errors.username = "Required";
-  } else if (values.username.length > 15) {
-    errors.username = "Must be 15 characters or less";
-  }
-
-  if (!values.password) {
-    errors.password = "Required";
-  } else if (values.password.length > 20) {
-    errors.password = "Must be 20 characters or less";
-  }
-
-  if (!values.confirmPassword) {
-    errors.confirmPassword = "Required";
-  } else if (values.password != values.confirmPassword) {
-    errors.confirmPassword = "Passward did not match";
-  } else if (values.confirmPassword.length > 20) {
-    errors.confirmPassword = "Must be 20 characters or less";
-  }
-
-  if (!values.phone) {
-    errors.phone = "Required";
-  } else if (values.phone.length != 10) {
-    errors.phone = "Must be 20 characters or less";
-  }
-  return errors;
-};
-
 function SignUp() {
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.email) {
+      errors.email = "Required";
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = "Invalid email address";
+    }
+
+    if (!values.username) {
+      errors.username = "Required";
+    } else if (values.username.length > 15) {
+      errors.username = "Must be 15 characters or less";
+    }
+
+    if (!values.password) {
+      errors.password = "Required";
+    } else if (values.password.length > 20) {
+      errors.password = "Must be 20 characters or less";
+    }
+
+    if (!values.confirmPassword) {
+      errors.confirmPassword = "Required";
+    } else if (values.password != values.confirmPassword) {
+      errors.confirmPassword = "Passward did not match";
+    } else if (values.confirmPassword.length > 20) {
+      errors.confirmPassword = "Must be 20 characters or less";
+    }
+
+    if (!values.phone) {
+      errors.phone = "Required";
+    } else if (!/^[1-9]+[0-9]{9}$/i.test(values.phone)) {
+      errors.phone = "Invalid Phone number";
+    } else if (values.phone.length != 10) {
+      errors.phone = "Must be 10 digits";
+    }
+    return errors;
+  };
+
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -50,13 +54,16 @@ function SignUp() {
     },
     validate,
     onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+      console.log(values);
+      alert("form submite successful");
     },
   });
+  // console.log(formik.errors);
   return (
     <div className="signUp">
       <img className="signUp__backgroundImage" src="/images/signUpBg.jpg" />
       <form className="signUp__form" onSubmit={formik.handleSubmit}>
+        <div className="signUp__title">SIGN UP</div>
         <div className="signUp__fields">
           <div className="signUp__form--inputArea">
             <label className="signUp__label">Email Address</label>
@@ -65,10 +72,11 @@ function SignUp() {
               name="email"
               type="email"
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               value={formik.values.email}
             />
           </div>
-          {formik.errors.email ? (
+          {formik.touched.email && formik.errors.email ? (
             <div className="signUp--error">{formik.errors.email}</div>
           ) : null}
         </div>
@@ -80,25 +88,27 @@ function SignUp() {
               name="username"
               type="text"
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               value={formik.values.username}
             />
           </div>
-          {formik.errors.username ? (
+          {formik.touched.username && formik.errors.username ? (
             <div className="signUp--error">{formik.errors.username}</div>
           ) : null}
         </div>
         <div className="signUp__fields">
           <div className="signUp__form--inputArea">
-            <label className="signUp__label">Passward</label>
+            <label className="signUp__label">Password</label>
             <input
               className="signUp__input"
               name="password"
               type="password"
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               value={formik.values.password}
             />
           </div>
-          {formik.errors.password ? (
+          {formik.touched.password && formik.errors.password ? (
             <div className="signUp--error">{formik.errors.password}</div>
           ) : null}
         </div>
@@ -110,10 +120,11 @@ function SignUp() {
               type="password"
               name="confirmPassword"
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               value={formik.values.confirmPassword}
             />
           </div>
-          {formik.errors.confirmPassword ? (
+          {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
             <div className="signUp--error">{formik.errors.confirmPassword}</div>
           ) : null}
         </div>
@@ -125,9 +136,13 @@ function SignUp() {
               name="phone"
               type="text"
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               value={formik.values.phone}
             />
           </div>
+          {formik.touched.phone && formik.errors.phone ? (
+            <div className="signUp--error">{formik.errors.phone}</div>
+          ) : null}
         </div>
         <button className="signUp__button" type="submit">
           SUBMIT
