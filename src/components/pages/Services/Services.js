5abd7b5cccb75ef5ff6897b686517129fdaacd64
gifty <jasmeetsingh200801@gmail.com>
2021-03-03 21:31:53 +0530

    added services page

--- src/components/pages/Services/Services.js
+++ src/components/pages/Services/Services.js
@@ -1,13 +1,51 @@
 import React from "react";
+import CardItem from "../../CardItems";
 import "./Services.css";
 
 function Services() {
   return (
     <div className="services">
-      <div className="ss1">services</div>
-      <div className="ss2">services</div>
-      <div className="ss3">services</div>
-      <div className="ss4">services</div>
+      <div className="services__heading f-head">
+        Our Services to ensure your welfare
+      </div>
+      <div className="services__container">
+        <div className="services__wrapper">
+          <ul className="services__items">
+            <CardItem
+              src="images/hotel.jpg"
+              text="Explore some of the best options of a stay"
+              label="Hotels"
+              path="/services"
+            />
+            <CardItem
+              src="images/meal.jpg"
+              text="Food that is die for"
+              label="Meals"
+              path="/services"
+            />
+          </ul>
+          <ul className="services__items">
+            <CardItem
+              src="images/flight.jpg"
+              text="FLy to your Dream Destination in style"
+              label="Flights"
+              path="/services"
+            />
+            <CardItem
+              src="images/train.jpg"
+              text="Experience the area with a tour in train"
+              label="Trains"
+              path="/services"
+            />
+            <CardItem
+              src="images/bus.jpg"
+              text="Unlock the joy with the best buses to for travel"
+              label="Bus"
+              path="/services"
+            />
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
